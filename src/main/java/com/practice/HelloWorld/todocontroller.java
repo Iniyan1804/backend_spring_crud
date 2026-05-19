bb19f0ca1b8b79dd32914b91012743e672ad7ba4
Iniyan1804 <iniyan.m1842006@gmail.com>
2026-05-19 15:16:01 +0530

    cross orgin changed

--- src/main/java/com/practice/HelloWorld/todocontroller.java
+++ src/main/java/com/practice/HelloWorld/todocontroller.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/todo")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class todocontroller {
     @Autowired
     private todoservise todoservice;
